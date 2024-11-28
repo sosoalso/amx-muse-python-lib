c15423514beb76b166524d064213d38bf38138f3
sosoalso <sosoalso@gmail.com>
2024-11-28 14:11:37 +0900

    241128-1411

--- lib_tp.py
+++ lib_tp.py
@@ -26,42 +26,42 @@ def tp_show_watcher(tp, index_port, index_btn):
 # ---------------------------------------------------------------------------- #
 def tp_add_watcher(tp, index_port, index_btn, callback):
     # if tp_get_device_state(tp) is False:
-        # return
-    # tp_clear_watcher(tp, index_port, index_btn)
+    # return
+    tp_clear_watcher(tp, index_port, index_btn)
     try:
         tp.port[index_port].button[index_btn].watch(callback)
     except Exception as e:
         print(f"Error adding watcher: {e}")
 
 
-# def tp_clear_watcher(tp, index_port, index_btn):
-#     if tp_get_device_state(tp) is False:
-#         return
-#     if isinstance(tp.port[index_port].button[index_btn].pythonWatchers, list):
-#         try:
-#             tp.port[index_port].button[index_btn].pythonWatchers.clear()
-#         except Exception as e:
-#             print(f"Error removing watcher: {e}")
+def tp_clear_watcher(tp, index_port, index_btn):
+    # if tp_get_device_state(tp) is False:
+    #     return
+    if isinstance(tp.port[index_port].button[index_btn].pythonWatchers, list):
+        try:
+            tp.port[index_port].button[index_btn].pythonWatchers.clear()
+        except Exception as e:
+            print(f"Error removing watcher: {e}")
 
 
 def tp_add_watcher_level(tp, index_port, index_level, callback):
     # if tp_get_device_state(tp) is False:
-        # return
-    # tp_clear_watcher_level(tp, index_port, index_level)
+    # return
+    tp_clear_watcher_level(tp, index_port, index_level)
     try:
         tp.port[index_port].button[index_level].watch(callback)
     except Exception as e:
         print(f"Error adding watcher: {e}")
 
 
-# def tp_clear_watcher_level(tp, index_port, index_level):
-#     if tp_get_device_state(tp) is False:
-#         return
-#     if isinstance(tp.port[index_port].level[index_level].pythonWatchers, list):
-#         try:
-#             tp.port[index_port].level[index_level].pythonWatchers.clear()
-#         except Exception as e:
-#             print(f"Error removing watcher: {e}")
+def tp_clear_watcher_level(tp, index_port, index_level):
+    # if tp_get_device_state(tp) is False:
+    #     return
+    if isinstance(tp.port[index_port].level[index_level].pythonWatchers, list):
+        try:
+            tp.port[index_port].level[index_level].pythonWatchers.clear()
+        except Exception as e:
+            print(f"Error removing watcher: {e}")
 
 
 # ---------------------------------------------------------------------------- #
@@ -78,9 +78,6 @@ def tp_send_command(tp, index_port, command_string):
 
 # ---------------------------------------------------------------------------- #
 def tp_set_button(tp, index_port, index_btn, value):
-    """
-    tp_set_button 함수는 tp 객체의 모든 요소에 대해 지정된 port와 btn의 상태[켜짐|꺼짐] 를 설정합니다.
-    """
     if tp_get_device_state(tp) is False:
         # print(f"warn tp_set_button() {tp=} {index_port=} {index_btn=} Device not running")
         return
@@ -95,18 +92,12 @@ def tp_set_button(tp, index_port, index_btn, value):
 
 # ---------------------------------------------------------------------------- #m
 def tp_set_button_in_range(tp, port, index_btn_start, index_btn_range, index_condition):
-    """
-    tp_set_button_in_range 함수는 tp 객체의 모든 요소에 대해 지정된 port와 btn의 상태[켜짐|꺼짐] 를 설정합니다.
-    """
     for i in range(index_btn_start, index_btn_start + index_btn_range + 1):
         tp_set_button(tp, port, i, index_condition == (i - index_btn_start + 1))
 
 
 # ---------------------------------------------------------------------------- #
 def tp_send_level(tp, index_port, index_lvl, value):
-    """
-    tp_send_level 함수는 tp 객체의 모든 요소에 대해 지정된 port와 idx_btn에 value를 전송합니다.
-    """
     if tp_get_device_state(tp) is False:
         # print(f"warn tp_send_level() {tp=} {index_port=} {index_lvl=} Device not running")
         return
@@ -121,25 +112,16 @@ def tp_send_level(tp, index_port, index_lvl, value):
 
 # ---------------------------------------------------------------------------- #
 def convert_text_to_unicode(text):
-    """
-    convert_text_to_unicode 함수는 주어진 텍스트를 유니코드 아스키로 변환하여 반환합니다.
-    """
     return "".join(format(ord(char), "04X") for char in text)
 
 
 # ---------------------------------------------------------------------------- #
 def tp_set_button_text_unicode(tp, index_port, index_addr, text):
-    """
-    tp_set_button_text_unicode 함수는 tp 객체의 모든 요소에 대해 지정된 port, index_addr, text를 이용하여 버튼에 유니코드 텍스트를 표시합니다.
-    """
     tp_send_command(tp, index_port, f"^UNI-{index_addr},0,{convert_text_to_unicode(text)}")
 
 
 # ---------------------------------------------------------------------------- #
 def tp_set_button_text(tp, index_port, index_addr, text):
-    """
-    tp_set_button_text 함수는 tp 객체의 모든 요소에 대해 지정된 port, index_addr, text를 이용하여 버튼에 텍스트를 표시합니다.
-    """
     tp_send_command(tp, index_port, f"^TXT-{index_addr},0,{text}")
 
 
