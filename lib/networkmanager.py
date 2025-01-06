da33e78be00929776aefde8a51a2b7e826f4af8e
sosoalso <sosoalso@gmail.com>
2025-01-06 16:51:23 +0900

    250106-1651

--- lib/networkmanager.py
+++ lib/networkmanager.py
@@ -6,10 +6,82 @@ from types import SimpleNamespace
 
 from lib.eventmanager import EventManager
 
+# ---------------------------------------------------------------------------- #
+BUFFER_SIZE = 1024
+# ---------------------------------------------------------------------------- #
+
+# # ---------------------------------------------------------------------------- #
+# class TCPServer(EventManager):
+#     def __init__(self, port, buffer_size=BUFFER_SIZE, connections=10):
+#         super().__init__("received")
+#         self.config = {"port": port, "buffer_size": buffer_size, "connections": connections}
+#         self.port = port
+#         self.connections = connections
+#         self.clients = []
+#         self.server = None
+#         self.executor = ThreadPoolExecutor(max_workers=connections + 2)
+#         self.lock = threading.Lock()
+#         self.running = False
+#         self.receive = self.ReceiveHandler(self)
+
+#     class ReceiveHandler:
+#         def __init__(self, client):
+#             self.client = client
+
+#         def listen(self, listener):
+#             self.client.add_event_handler("received", listener)
+
+#     def get_client(self, client, addr):
+#         while self.running:
+#             data = client.recv(1024)
+#             if data:
+#                 event = SimpleNamespace()
+#                 event.arguments = {"data": data, "address": addr}
+#                 self.trigger_event("received", event)
+#             else:
+#                 client.close()
+#                 self.clients.remove(client)
+#                 print(f"Connection closed from {addr}")
+
+#     def send_clients(self, data):
+#         if not self.clients:
+#             print("No clients connected")
+#             return
+#         for client in self.clients:
+#             client.sendall(data)
+
+#     def send_client(self, client, data):
+#         if client in self.clients:
+#             client.sendall(data)
+
+#     def start_server_thread(self):
+#         self.running = True
+#         self.executor.submit(self._start_server)
+
+#     def _start_server(self):
+#         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         self.server.bind((socket.gethostname(), self.port))
+#         self.server.listen(self.connections)  # max connections
+#         # print(f"TCP Server Listening on {self.host}:{self.port}")
+#         while self.running:
+#             try:
+#                 (client_sock, client_addr) = self.server.accept()
+#                 if client_sock:
+#                     self.clients.append(client_sock)
+#                     self.executor.submit(self.get_client, args=(client_sock, client_addr))
+#             except (socket.error, socket.timeout):
+#                 if not self.running:
+#                     break
+
+#     def stop_server(self):
+#         self.running = False
+#         self.server.close()
+#         self.executor.shutdown(wait=True)
+#         # print(f"TCP Server Disconnected from {self.host}:{self.port}")
+
 
 # ---------------------------------------------------------------------------- #
 class TcpClient(EventManager):
-    BUFFER_SIZE = 1024
 
     def __init__(self, name, ip, port, reconnect=True, time_reconnect=5, buffer_size=BUFFER_SIZE):
         super().__init__("connected", "received")
@@ -129,12 +201,12 @@ class UdpClient:
 
 # ---------------------------------------------------------------------------- #
 class UdpServer(EventManager):
-    BUFFER_SIZE = 1024
 
-    def __init__(self, port):
+    def __init__(self, port, buffer_size=BUFFER_SIZE):
         super().__init__("received")
         self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.BUFFER_SIZE = buffer_size
+        self.socket = None
         self.socket.bind(("", self.port))
         self.executor = ThreadPoolExecutor(max_workers=2)
         self.running = False
@@ -152,6 +224,7 @@ class UdpServer(EventManager):
         self.executor.submit(self._start_server)
 
     def _start_server(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         while self.running:
             try:
                 data, addr = self.socket.recvfrom(self.BUFFER_SIZE)
